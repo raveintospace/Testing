48577a7944451d1ef58b9494df06ebfac3dc41d9
Oriol Angelet <uriaeri@gmail.com>
2023-12-03 16:28:36 +0100

    Lesson 2, pt 5 - ToolbarItems for CreateNoteView

--- Testing/Testing/Core/Views/CreateNoteView.swift
+++ Testing/Testing/Core/Views/CreateNoteView.swift
@@ -18,20 +18,53 @@ struct CreateNoteView: View {
     
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
-                    TextField("", text: $title, prompt: Text("*Title"), axis: .vertical)
-                    TextField("", text: $text, prompt: Text("Text"), axis: .vertical)
-                } footer: {
-                    Text("*Title is compulsory")
+            createNoteForm
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        xMarkDismissButton
+                    }
+                    
+                    ToolbarItem {
+                        createNoteButton
+                    }
                 }
-            }
+                .navigationTitle("New note")
+                .navigationBarTitleDisplayMode(.large)
         }
-        .navigationTitle("New note")
-        .navigationBarTitleDisplayMode(.large)
     }
 }
 
 #Preview {
     CreateNoteView(viewModel: .init())
+}
+
+extension CreateNoteView {
+    
+    private var createNoteForm: some View {
+        Form {
+            Section {
+                TextField("", text: $title, prompt: Text("*Title"), axis: .vertical)
+                TextField("", text: $text, prompt: Text("Text"), axis: .vertical)
+            } footer: {
+                Text("*Title is compulsory")
+            }
+        }
+    }
+    
+    private var xMarkDismissButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "x.circle")
+        }
+    }
+    
+    private var createNoteButton: some View {
+        Button {
+            viewModel.createNoteWith(title: title, text: text)
+        } label: {
+            Text("Create Note")
+                .bold()
+        }
+    }
 }

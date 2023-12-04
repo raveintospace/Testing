68472652bd706d5c012da34e452e8d6cc204f8f1
Oriol Angelet <uriaeri@gmail.com>
2023-12-04 18:18:03 +0100

    Lesson 3, pt 1 - Unit testing, first test of Note model

---
+++ Testing/TestingTests/NoteTests.swift
@@ -0,0 +1,27 @@
+//
+//  NoteTests.swift
+//  NoteTests
+//
+//  Created by Uri on 4/12/23.
+//
+
+import XCTest
+@testable import Testing
+
+final class NoteTests: XCTestCase {
+
+    func test_Note_Initialization() {
+        // Given
+        let title = "Test title"
+        let text = "Test text"
+        let date = Date()
+        
+        // When
+        let note = Note(title: title, text: text, createdAt: date)
+        
+        // Then
+        XCTAssertEqual(note.title, title)
+        XCTAssertEqual(note.text, text)
+        XCTAssertEqual(note.createdAt, date)
+    }
+}

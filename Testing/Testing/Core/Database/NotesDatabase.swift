6784a0df6c75b325283304b763ad6a888569cde9
Oriol Angelet <uriaeri@gmail.com>
2023-12-12 18:49:18 +0100

    Lesson 4, pt 2 - NotesDatabase conforms to protocol and uses an error enum

--- Testing/Testing/Core/Database/NotesDatabase.swift
+++ Testing/Testing/Core/Database/NotesDatabase.swift
@@ -8,8 +8,25 @@
 import Foundation
 import SwiftData
 
-class NotesDatabase {
+enum DatabaseError: Error {
+    case errorInsert
+    case errorFetch
+    case errorUpdate
+    case errorRemove
+}
+
+protocol NotesDatabaseProtocol {
+    func insert(note: Note) throws
+    func fetchAll() throws -> [Note]
+    
+}
+
+class NotesDatabase: NotesDatabaseProtocol {
     static let shared: NotesDatabase = NotesDatabase()
+    
+    // false for production code
+    @MainActor
+    var container: ModelContainer = setupContainer(inMemory: false)
     
     private init() {}
     
@@ -24,6 +41,32 @@ class NotesDatabase {
         } catch {
             debugPrint("Error: \(error.localizedDescription)")
             fatalError("Database can't be created")
+        }
+    }
+    
+    // store note in memory
+    @MainActor
+    func insert(note: Note) throws {
+        container.mainContext.insert(note)
+        
+        do {
+            try container.mainContext.save()
+        } catch {
+            debugPrint("Error \(error.localizedDescription)")
+            throw DatabaseError.errorInsert
+        }
+    }
+    
+    // retrieve stored notes
+    @MainActor
+    func fetchAll() throws -> [Note] {
+        let fetchDescriptor = FetchDescriptor<Note>(sortBy: [SortDescriptor<Note>(\.createdAt)])
+        
+        do {
+            return try container.mainContext.fetch(fetchDescriptor)
+        } catch {
+            debugPrint("Error \(error.localizedDescription)")
+            throw DatabaseError.errorFetch
         }
     }
 }
